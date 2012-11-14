3d7753f70c13b5eeabec7040cb6c1cadde70498d
askhugo <hugo@grochau.com>
2012-11-14 11:51:24 -0200

    Add box2d library
    The box2d js/closure port was added
    The project folder is now src
    Launch and compile scripts were modified accordingly

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -4,7 +4,7 @@ cd ~/Documents/prog/projects/owntra/
 
 ../closure-library/closure/bin/build/closurebuilder.py \
 --root=../closure-library/ \
---root=owntra/ \
+--root=src/ \
 --namespace="owntra.Game" \
 --output_mode=compiled \
 --compiler_jar=../closure-library/compiler.jar \
--- scripts/launch-dev.sh
+++ scripts/launch-dev.sh
@@ -3,7 +3,7 @@
 cd ~/Documents/prog/projects/owntra
 
 ../closure-library/closure/bin/build/depswriter.py \
---root_with_prefix="owntra ../../../owntra/owntra" \
+--root_with_prefix="src ../../../owntra/src" \
 > deps.js
 
 google-chrome index.html
