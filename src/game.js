a9f20b5fe49943aa70e3b82de3d26b761ba1aa21
askhugo <hugo@grochau.com>
2013-01-12 23:08:43 -0200

    Add semicolon to the end of function declarations

--- src/game.js
+++ src/game.js
@@ -10,15 +10,15 @@ goog.require("goog.json")
 owntra.Game = function() {
     this.name = 'Owntra';
     this.myPlayer = new owntra.Player('Hugo');
-}
+};
 
 owntra.Game.prototype.getName = function() {
     return this.name;
-}
+};
 
 owntra.Game.prototype.getPlayer = function() {
     return this.myPlayer;
-}
+};
 
 var game = new owntra.Game();
 console.log('Game Started');
