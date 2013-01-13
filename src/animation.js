332fe9c1f4614ecab67fe806ddb4222f0b8f2ac5
askhugo <hugo@grochau.com>
2013-01-12 23:09:55 -0200

    Convert animation to closure

--- src/animation.js
+++ src/animation.js
@@ -1,28 +1,40 @@
-define(function () {
+goog.provide("owntra.Animation");
 
-    var Animation = Class.extend({
+/**
+ * Animation class
+ * @constructor
+ * @param {Integer} row    the row of the animation frames
+ * @param {Integer} frames the number of frames
+ * @param {Integer} speed  the speed of the animation
+ * @param {Boolean} loop   if the animation should loop 
+ */
+owntra.Animation = function(row, frames, speed, loop) {
+    this.row = row;
+    this.frames = frames;
+    this.speed = speed; // frames/second
+    this.loop = loop;
+    this.currentFrame = 0;
+    this.lastTime = 0;
+    this.running = true;
+};
 
-        init: function (row, frames, speed) {
-            this.row = row;
-            this.frames = frames;
-            this.speed = speed; // frames/second
-
-            this.currentFrame = 0;
-            this.lastTime;
-        },
-
-        tick: function() {
-            var d = new Date();
-            if (d.getMilliseconds - this.lastTime >= 1000 / this.speed) { // time difference >= delay between frames
-                this.currentFrame++;
-                if (this.currentFrame > this.frames) { // looping
-                    this.currentFrame = 0;
-                }
-                this.lastTime = d;
-            }
+/**
+ * Runs every time the frame needs to be updated
+ */
+owntra.Animation.prototype.tick = function() {
+    var d = new Date();
+    if (d.getMilliseconds - this.lastTime >= 1000 / this.speed && this.running) { // time difference >= delay between frames
+        this.currentFrame++;
+        if (this.currentFrame > this.frames) { // looping
+            if (this.loop)
+                this.currentFrame = 0;
+            else
+                this.running = false;
         }
+        this.lastTime = d.getMilliseconds;
+    }
+};
 
-    });
-
-    return Animation;
-});
+owntra.Animation.prototype.stop = function() {
+    this.running = false;
+};
