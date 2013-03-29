5154cfb82047f2da770dcc654c9902068a6b1faf
Hugo Grochau <hugo@grochau.com>
2013-03-29 20:36:10 -0300

    Remove closure library submodule hosted on github

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,47 +1,43 @@
 module.exports = function(grunt) {
 
-    grunt.initConfig({
+  grunt.initConfig({
 
-        closureBuilder: {
-            client_production: {
-                closureLibraryPath: 'closure-library',
-                root: ['src', 'closure-library/closure/goog', 'closure-library/third_party/closure/goog'],
-                inputs: 'src/game.js',
-                output_file: 'static/scripts/owntra.min.js',
-                compile: true,
-                compiler: 'bin/compiler.jar',
-                compiler_options: {
-                    compilation_level: 'ADVANCED_OPTIMIZATIONS',
-                    jscomp_warning: 'visibility', // Give warnings when accessing private or protected members
-                    process_closure_primitives: true // (goog.provide, goog.import, etc)
-                }
-            },
-            client_development: {
-                closureLibraryPath: 'closure-library',
-                root: ['src', 'closure-library/closure/goog', 'closure-library/third_party/closure/goog'],
-                inputs: 'src/game.js',
-                output_mode: 'script',
-                output_file: 'static/scripts/owntra.js'
-            }
+    closureBuilder: {
+      options: {
+        closureLibraryPath: 'closure-library',
+        compilerFile: 'bin/compiler.jar',
+        namespaces: 'owntra.Game'
+      },
+      client_development: {
+        src: ['src', 'closure-library/closure/goog', 'closure-library/third_party/closure/goog'],
+        dest: 'static/scripts/owntra.js'
+      },
+      client_production: {
+        options: {
+          compile: true,
         },
+        src: ['src', 'closure-library/closure/goog', 'closure-library/third_party/closure/goog'],
+        dest: 'static/scripts/owntra-min.js'
+      }
+    },
 
-        stylus: {
-            compile: {
-                options: {
-                    compress: true
-                },
-                files: {
-                    'static/styles/main.css': 'styles/main.styl'
-                }
-            }
+    stylus: {
+      compile: {
+        options: {
+          compress: true
+        },
+        files: {
+          'static/styles/main.css': 'styles/main.styl'
         }
-    });
+      }
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-closure-tools');
-    grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-closure-tools');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
 
-    grunt.registerTask('pro', 'closureBuilder:client_production');
-    grunt.registerTask('dev', 'closureBuilder:client_development');
-    grunt.registerTask('styles', 'stylus:compile');
-    grunt.registerTask('default', 'styles dev pro');
+  grunt.registerTask('pro', 'closureBuilder:client_production');
+  grunt.registerTask('dev', 'closureBuilder:client_development');
+  grunt.registerTask('styles', 'stylus:compile');
+  grunt.registerTask('default', 'styles dev pro');
 }
